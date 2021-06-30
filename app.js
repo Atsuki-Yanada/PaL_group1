1acf91a5db65f9fb4b4467dd47651f6c84c48c83
s1260145 <s1260145@u-aizu.ac.jp>
2021-06-30 09:44:26 +0900

    add comment task1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Return the appropriate verb and the difference between those prices.
 const getVerb = (price1str, price2str) => {
     const price1 = Number(price1str.substr(0, price1str.indexOf(' yen')));
     const price2 = Number(price2str.substr(0, price2str.indexOf(' yen')));
@@ -15,6 +15,7 @@ const getVerb = (price1str, price2str) => {
     return verb;
 }
 
+// Generate a sentence from the given "value1-5".
 const getSentence = (subject, season1, price1, season2, price2) => {
     if(getInputError(subject, season1, price1, season2, price2)){
         return 'Please enter all of the items.';
@@ -28,6 +29,7 @@ const getSentence = (subject, season1, price1, season2, price2) => {
     return sentence;
 }
 
+// Check that all values have been inputed.
 const getInputError = (subject, season1, price1, season2, price2) => {
     if (subject === '' || season1 === '' || price1 === '' || season2 === '' || price2 === '') {
         return true;
@@ -36,6 +38,7 @@ const getInputError = (subject, season1, price1, season2, price2) => {
     }
 }
 
+// Listen if a button on the page is pressed and embeds the sentences from the given "value1-5" into the HTML.
 $('#task1-submit-btn').addEventListener('click', () => {
     const task1Form = document.forms['task1Form'];
     const subject = task1Form.elements['subject'].value;
