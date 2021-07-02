4f7d676738bdc4c1dac95a59d0113ba1bb03e9d0
s1260145 <s1260145@u-aizu.ac.jp>
2021-07-02 12:25:26 +0900

    add orange task

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // Return the appropriate verb and the difference between those prices.
-const getVerb = (price1str, price2str) => {
+const getSentence = (subject, season1, price1str, season2, price2str) => {
     const price1 = Number(price1str.substr(0, price1str.indexOf(' yen')));
     const price2 = Number(price2str.substr(0, price2str.indexOf(' yen')));
 
-    let verb;
+    let sentence;
     if (price1 - price2 < 0) {
-        verb = 'increased by ' + Math.abs(price1 - price2) + ' yen';
+        sentence = subject + ' increased by ' + Math.abs(price1 - price2) + ' yen' + ' from ' +
+        price1str + ' in ' + season1 + ' to ' + price2str + ' in ' + season2 + '.';
     } else if (price1 - price2 > 0) {
-        verb = 'decreased by ' + Math.abs(price1 - price2) + ' yen';
+        sentence = subject + ' decreased by ' + Math.abs(price1 - price2) + ' yen' + ' from ' +
+        price1str + ' in ' + season1 + ' to ' + price2str + ' in ' + season2 + '.';
     } else {
-        verb = 'remain';
+        sentence = subject + ' remained stable at ' + price1str + '.';
     }
-    console.log(verb);
-    return verb;
+
+    console.log(sentence);
+    return sentence;
 }
 
 // Generate a sentence from the given "value1-5".
-const getSentence = (subject, season1, price1, season2, price2) => {
+const getSentences = (subject, season1, price1, season2, price2) => {
     if(getInputError(subject, season1, price1, season2, price2)){
         return 'Please enter all of the items.';
     }
 
     let sentence;
-    const verb = getVerb(price1, price2);
-    sentence = subject + ' ' + verb + ' from ' +
-        price1 + ' in ' + season1 + ' to ' + price2 + ' in ' + season2 + '.';
     console.log(sentence);
-    return sentence;
+    return getSentence(subject, season1, price1, season2, price2);
 }
 
 // Check that all values have been inputed.
@@ -46,7 +46,9 @@ $('#task1-submit-btn').addEventListener('click', () => {
     const price1 = task1Form.elements['price1'].value;
     const season2 = task1Form.elements['season2'].value;
     const price2 = task1Form.elements['price2'].value;
+    
+    const strs = []
 
-    $('#task1-result').innerHTML = getSentence(subject, season1, price1, season2, price2);
+    $('#task1-result').innerHTML = getSentences(subject, season1, price1, season2, price2);
 });
 
